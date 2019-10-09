1e37ed2b441c92dc3c3230bed224d9cfd03677a2
Ilya Mukhortov <>
2019-10-09 10:35:19 +1000

    pass

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name='smlp-drf',
     version='0.1b',
     packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
 )
